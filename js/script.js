49d38cc22c8360db4405923d528aab365bd2dfa7
Kelvin <kelvin.yeung@rocketmail.com>
2023-03-15 23:50:29 +0000

    auto spacing, added js file

---
+++ js/script.js
@@ -0,0 +1,19 @@
+(function (window){
+    
+  })(window);
+
+
+function leftMinusPressed(){
+    console.log("leftMinusPressed");
+}
+function rightMinusPressed(){
+    console.log("rightMinusPressed");
+}
+
+function openSettingsPanel(){
+
+}
+
+function closeSettingsPanel(){
+
+}
